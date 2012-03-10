ca41576c613a7c09b4a19249a09a4b58666ff808
taylorjb <taylorjb@ee9ebe8e-1be5-6e41-b6f4-7136b78df094>
2012-03-10 22:32:55 +0000

    Unused import and version change

--- src/com/evosysdev/bukkit/taylorjb/simplemod/SimpleModListener.java
+++ src/com/evosysdev/bukkit/taylorjb/simplemod/SimpleModListener.java
@@ -5,7 +5,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerLoginEvent.Result;
 
